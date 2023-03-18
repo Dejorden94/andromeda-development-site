79956dca781d6b73dde9034188216b854a492316
Dejorden <d.moerman@hotmail.nl>
2023-03-18 13:18:02 +0100

    add page layout

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card style="min-height: 100vh;">
+    <v-layout>
+      <v-app-bar title="Andormeda Development">
+      </v-app-bar>
+
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="min-height: 100vh;">
+
+      </v-main>
+    </v-layout>
+  </v-card>
+
+  <ProjectView />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import ProjectView from '../Organisms/ProjectOrganism.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    ProjectView,
+  },
+});
+</script>
