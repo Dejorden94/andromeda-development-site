3f20ea37f4dbcd5ccebdb016b9bad70930d9e85d
Dejorden <d.moerman@hotmail.nl>
2023-05-05 14:49:33 +0200

    add information to about page

--- src/components/organisms/AboutOrganism.vue
+++ src/components/organisms/AboutOrganism.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <article class="about">
+    <section>
+      <h2>About Dejorden Moerman</h2>
+      <img src="../../img/jpg/Dejorden.JPG" alt="The man, the myth, the legend, Dejorden Moerman.">
+    </section>
+  </article>
 </template>
+
+<script>
+
+export default {
+
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
-    align-items: center;
+  }
+
+  .about>section {
+    height: 60vh;
+  }
+
+  .about>section>img {
+    height: 100%;
   }
 }
 </style>
